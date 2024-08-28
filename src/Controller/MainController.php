d1a9fccdb5572b4625909b616230ea4294b367ee
adraud2023 <alexis.draud2023@campus-eni.fr>
2024-08-28 14:21:47 +0200

    MainController et base twig

---
+++ src/Controller/MainController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_main')]
+    public function home(): Response
+    {
+        return $this->render('home/home.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/welcome', name: 'app_welcome')]
+    public function welcome(): Response
+    {
+        if ($this->getUser()) {
+            $this->addFlash('success', 'Hello ' . $this->getUser()->getUserIdentifier() . ' !');
+        }
+
+        return $this->redirectToRoute('app_main');
+    }
+
+
+    #[Route('/aboutus', name: 'app_aboutus')]
+    public function aboutUs(): Response
+    {
+        $filePath = $this->getParameter('kernel.project_dir') . '/data/team.json';
+
+        $jsonContent = file_get_contents($filePath);
+        $teamData = json_decode($jsonContent, true);
+
+        return $this->render('home/aboutus.html.twig', [
+            'team' => $teamData
+        ]);
+    }
+}
